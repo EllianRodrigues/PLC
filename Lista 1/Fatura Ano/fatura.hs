a5eb2ce5bc4eb87b024bceef1ef954ddeaa30c1b
EllianRodrigues <ellian.rodrigues.ti@gmail.com>
2025-09-26 00:17:24 -0300

    dificil

---
+++ Lista 1/Fatura Ano/fatura.hs
@@ -0,0 +1,27 @@
+import Data.List (foldl')
+
+split :: Char -> String -> [String]
+split _ "" = []
+split sep s =
+  let (parte, resto) = break (== sep) s
+  in parte : case resto of
+       []     -> []
+       (_:xs) -> split sep xs
+
+toRecords :: [String] -> [(String,String,String)]
+toRecords [] = []
+toRecords (d:t:v:xs) = (d,t,v) : toRecords xs
+toRecords _ = []
+
+logMes :: String -> String -> Double
+logMes mes fatura = foldl' (+) 0.0 valores
+  where
+    partes    = split ';' fatura
+    registros = toRecords partes
+    valores   = [ read v :: Double | (d,_,v) <- registros, last (words d) == mes ]
+
+main = do
+    a <- getLine
+    b <- getLine
+    let result = logMes a b
+    print result
