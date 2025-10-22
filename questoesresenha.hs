d5512c596f9297263c65c0dae2f1b2ede9a96cb4
EllianRodrigues <ellian.rodrigues.ti@gmail.com>
2025-10-22 12:14:02 -0300

    quase finalizada prova 2019_1

--- questoesresenha.hs
+++ questoesresenha.hs
@@ -96,3 +96,92 @@ convertTimeaux :: Int -> Int -> TimeOfDay
 convertTimeaux h m | h >= 12 = PM (h-12) m
                    | otherwise = AM h m
 
+--------------------------------------------------------------------------------------------------------------------------------------
+
+{-Escreva uma função que funcione como uma função "meio" ou "mid" do Excel: ela recebe 3 parâmetros, uma String e dois números ( digamos x e y), e retorna a substring que começa no primeiro índice
+ ( o primeiro número, x )e contém y caracteres. A primeira posição na lista tem índice 1 ( um). 
+Casos inválidos retornam a lista vazia (por exemplo, números negativos ou inicio fora do tamanho da lista). Se a lista contiver menos caracteres que o solicitado deve ser retornado o que for possível.-}
+
+mid :: String -> Int -> Int -> String
+mid str x y | x <= 0 = ""
+            | y <= 0 = ""
+            | x > length str = ""
+            | otherwise = take y (drop (x-1) str)
+
+{-2) (2.5) Escreva uma função localizar, que indica a posição  de início de uma String dentro de outra. 
+Caso não seja encontrada ou a String de busca seja vazia, deve ser retornado o valor 0 (zero). 
+A contagem das posições começa no valor 1 para o primeiro caracter. Deve ser retornada a posicao da primeira ocorrencia, se houver mais de uma.
+Exemplos: localizar "abc" "xyz12abrt" ----> 0
+          localizar "abc" "aaabrsabcfr" --> 7
+          localizar "aab" "aacrabceaabc" -> 9
+          localizar "" "aacrabceaabc" ----> 0-}
+
+localizar :: String -> String -> Int
+localizar "" _ = 0
+localizar sub str = localizaraux sub str 1
+
+localizaraux :: String -> String -> Int -> Int
+localizaraux _ [] _ = 0
+localizaraux sub str@(x:xs) n  | take (length sub) str == sub = n
+                               | otherwise = localizaraux sub xs (n+1)
+
+{-3) Uma fita infinita de papel pode ser representada por uma lista de caracteres. Esta fita possui uma cabeça de leitura/escrita que le ou escreve na posição atual da fita. 
+Esta fita pode ser lida ou escrita (alterada) pelos seguintes comandos:
+ParaFrente -- Que recebe um inteiro N como parâmetro e move a posição de leitura N posições para frente;
+ParaTras -- que recebe um inteiro N como parâmetro e move a posição de leitura N posições para tras;
+Escreva -- que recebe um caracter como parametro e escreve o caracter naquela posicao da fita, substituindo o caracter que estiver naquela posicao;
+A fita comeca na posicao de numero 1.
+Nao é preciso tratar o caso da fita se mover para uma posicao invalida (menor que 1).
+
+data Comando = ParaFrente Int
+             | ParaTras Int
+             | Escreva Char
+             deriving (Show, Eq)
+             
+3.1) (2.0) Esreva uma funcao que diga a posicao final da fita, apos receber uma lista de comandos.
+Exemplos:
+posicaofinal "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1, Escreva 'y', ParaTras 1] ---> 6
+posicaofinal "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1, Escreva 'y']  --------------> 7
+posicaofinal "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1]  ---------------------------> 7
+posicaofinal "abcdefghijklmno" [] -------------------------------------------------------------------> 1
+posicaofinal :: String -> [Comando] -> Int 
+
+-}
+
+data Comando = ParaFrente Int
+             | ParaTras Int
+             | Escreva Char
+             deriving (Show, Eq)
+
+posicaofinal :: String -> [Comando] -> Int
+posicaofinal str comandos  = 1 + posicaofinalaux comandos
+
+posicaofinalaux :: [Comando] -> Int
+posicaofinalaux [] = 0
+posicaofinalaux (ParaFrente n : cauda) = n + posicaofinalaux cauda
+posicaofinalaux (ParaTras n : cauda) = (-n) + posicaofinalaux cauda
+posicaofinalaux (Escreva _ : cauda) = posicaofinalaux cauda
+
+{-
+
+3.2) (2.0) Escreva uma funcao que, dada uma lista de comandos, retorne o caracter da posicao final da cabeca de leitura, apos comandos.
+Exemplos:
+interprete "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1, Escreva 'y', ParaTras 1] ---> 'x'
+interprete "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1, Escreva 'y']  --------------> 'y'
+interprete "abcdefghijklmno" [ParaFrente 5, Escreva 'x', ParaFrente 1]  ---------------------------> 'g'
+interprete "abcdefghijklmno" [] -------------------------------------------------------------------> 'a'
+interprete :: String -> [Comando] -> Char
+
+-}
+
+interprete :: String -> [Comando] -> Char
+interprete str comandos = interpreteaux str comandos 1
+
+interpreteaux :: String -> [Comando] -> Int -> Char
+interpreteaux str [] pos = head (drop (pos - 1) str)
+interpreteaux str (ParaFrente n: cauda) pos = interpreteaux str cauda (pos + n)
+interpreteaux str (ParaTras n: cauda) pos = interpreteaux str cauda (pos - n)
+interpreteaux str (Escreva c: cauda) pos = interpreteaux (replaceChar str (pos-1) c) cauda pos
+
+replaceChar :: String -> Int -> Char -> String
+replaceChar str index newChar = take index str ++ [newChar] ++ drop (index + 1) str
